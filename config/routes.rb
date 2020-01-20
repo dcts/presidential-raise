6e74743d3066c9bc3f3d5df41aceb53fc45f944c
David Friedl <david.friedl@whu.edu>
2020-01-20 18:47:34 +0100

    route to terms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #home
   root to: 'pages#home'
 
-  # GET / POST SYNTAX
-  # get '/users/:id', to: "users#reset_password", as: "reset_password"
-  # post '/tasks/do_smth', to: "tasks#do_smth", as: "do_smth"
+  #pages
+  get '/terms', to: 'pages#terms', as: 'terms'
 end
